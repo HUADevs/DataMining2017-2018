ee2b323699e4f77fbb46ab605adee148c3a39e79
Georgios Alexandridis <galexandridis96@gmail.com>
2017-11-30 23:08:00 +0200

    changed X to x, added plot for discretization

--- Code/preprocessing_tests.py
+++ Code/preprocessing_tests.py
@@ -4,13 +4,15 @@ import pandas as pd
 from sklearn.preprocessing import Imputer
 
 from sklearn.neighbors import LocalOutlierFactor
-from sklearn.covariance import EllipticEnvelope
 from sklearn.ensemble import IsolationForest
 
-from sklearn.preprocessing import StandardScaler, MinMaxScaler, RobustScaler, MaxAbsScaler
+from sklearn.preprocessing import StandardScaler, MinMaxScaler, RobustScaler, MaxAbsScaler, QuantileTransformer
 
 from sklearn.feature_selection import SelectKBest
 from sklearn.feature_selection import chi2, f_classif, mutual_info_classif
+
+import seaborn as sns
+from matplotlib import pyplot as plt
 
 from sklearn.model_selection import train_test_split
 
@@ -20,24 +22,24 @@ def read_data(csv_file):
     df = pd.read_csv(csv_file, na_values='?')
 
     # split to independent features (X) and target feature (y)
-    X = df.drop('X65', axis=1)
+    x = df.drop('X65', axis=1)
     y = df['X65']
 
-    return X, y
+    return x, y
 
 
-def impute_missing_values(X, strategy='most_frequent'):
+def impute_missing_values(x, strategy='most_frequent'):
 
     # impute missing values
     imp = Imputer(missing_values='NaN', strategy=strategy, axis=0)
     print('Imputing missing values with strategy: {strategy}...'.format(strategy=strategy))
-    imp.fit(X)
-    X = pd.DataFrame(data=imp.transform(X), columns=X.columns)
+    imp.fit(x)
+    x = pd.DataFrame(data=imp.transform(x), columns=x.columns)
 
-    return X
+    return x
 
 
-def detect_and_remove_outliers(X, y, clf='LOF'):
+def detect_and_remove_outliers(x, y, clf='LOF'):
     classifiers = {
         'LOF': LocalOutlierFactor(n_neighbors=35),
         'IF': IsolationForest(n_estimators=100),
@@ -47,11 +49,11 @@ def detect_and_remove_outliers(X, y, clf='LOF'):
     est = classifiers[clf]
     print('Detecting and removing outliers using {classifier}...'.format(classifier=clf))
     if clf == 'LOF':
-        outliers = est.fit_predict(X, y)
+        outliers = est.fit_predict(x, y)
     else:
-        outliers = est.fit(X, y).predict(X)
+        outliers = est.fit(x, y).predict(x)
     #  keep only non-outliers (predicted values outliers=-1, non-outliers=1)
-    new_X = X[outliers == 1]
+    new_X = x[outliers == 1]
     new_y = y[outliers == 1]
 
     return new_X, new_y
@@ -69,7 +71,7 @@ def find_outliers_tukey(x):
     return outliers_indices, outliers_values
 
 
-def normalize_data(X, scaler='Standard'):
+def normalize_data(x, scaler='Standard'):
     scalers = {
         # StandardScaler not used on sparse data but if needed with_mean=False
         'Standard': StandardScaler(copy=True, with_mean=True, with_std=True),
@@ -77,17 +79,17 @@ def normalize_data(X, scaler='Standard'):
         'MinMax': MinMaxScaler(),  # range [0,1]
         'MaxAbs': MaxAbsScaler(),  # range [-1,1]
         # cannot be fitted to sparse inputs
-        'Robust': RobustScaler(),
+        'Robust': RobustScaler()
 
     }
 
     print('Scaling data using {scaler} Scaler...'.format(scaler=scaler))
-    X = pd.DataFrame(data=scalers[scaler].fit_transform(X), columns=X.columns)
+    x = pd.DataFrame(data=scalers[scaler].fit_transform(x), columns=x.columns, index=x.index.values)
 
-    return X
+    return x
 
 
-def select_best_features(X, y, n_features=5, method='chi2'):
+def select_best_features(x, y, n_features=5, method='chi2'):
     methods = {
         # Compute chi-squared stats between each non-negative feature and class
         'chi2': SelectKBest(chi2, k=n_features),
@@ -98,33 +100,43 @@ def select_best_features(X, y, n_features=5, method='chi2'):
         'mutual_info_classif': SelectKBest(mutual_info_classif, k=n_features)
     }
 
-    X = pd.DataFrame(data=methods[method].fit_transform(X, y), columns=X.columns[methods[method].get_support()])
+    print('Selecting features using {method}...'.format(method=method))
+    x = pd.DataFrame(data=methods[method].fit_transform(x, y), columns=x.columns[methods[method].get_support()], index=x.index.values)
 
-    return X
+    return x
+
+
+def plot(x, y):
+    df = pd.concat([x, y], axis=1)
+    print(df.head())
+    sns.pairplot(df, hue='X65')
+    plt.show()
 
 
 def preprocessing():
 
-    X, y = read_data(csv_file='companydata.csv')
+    x, y = read_data(csv_file='companydata.csv')
 
     # # check NaN values count by column
     # print('Missing values count:')
     # print(X.isnull().sum().sort_values(ascending=False).head())
-    X = impute_missing_values(X, strategy='most_frequent')  # strategies={mean, median, most_frequent}
+    x = impute_missing_values(x, strategy='most_frequent')  # strategies={mean, median, most_frequent}
     # # check NaN values count by column after imputation
     # print('Missing values count after imputer:')
     # print(X.isnull().sum().sort_values(ascending=False).head())
 
-    print('Dataset\'s rows with outliers: {shape}'.format(shape=X.shape[0]))
-    X, y = detect_and_remove_outliers(X, y, clf='IF')  # clf={IF(IsolationForest), LOF(LocalOutlierFactor)}
-    print('Dataset\'s rows after removing outliers: {shape}'.format(shape=X.shape[0]))
+    print('Dataset\'s rows with outliers: {shape}'.format(shape=x.shape[0]))
+    x, y = detect_and_remove_outliers(x, y, clf='LOF')  # clf={IF(IsolationForest), LOF(LocalOutlierFactor)}
+    print('Dataset\'s rows after removing outliers: {shape}'.format(shape=x.shape[0]))
 
-    X = normalize_data(X, scaler='MinMax')
+    x = normalize_data(x, scaler='MinMax')  # methods={Standard, MinMax, MaxAbs, Robust, Quantile}
 
-    print('Dataset\'s columns before feature selection: {shape}'.format(shape=X.shape[1]))
-    X = select_best_features(X, y, n_features=5, method='mutual_info_classif')  # methods={chi2, f_classif, mutual_info_classif}
-    print('Dataset\'s columns after feature selection: {shape}'.format(shape=X.shape[1]))
-    print(X.head())
+    print('Dataset\'s columns before feature selection: {shape}'.format(shape=x.shape[1]))
+    x = select_best_features(x, y, n_features=5, method='mutual_info_classif')  # methods={chi2, f_classif, mutual_info_classif}
+    print('Dataset\'s columns after feature selection: {shape}'.format(shape=x.shape[1]))
+    print(x.head())
+
+    plot(x, y)
 
     # split to training and testing data
     # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.2)
